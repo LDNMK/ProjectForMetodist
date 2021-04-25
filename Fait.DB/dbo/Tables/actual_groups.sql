9aec7a4d40c3aa158f4cfad79953981c55f1db83
AlecCore <alocha2001@gmail.com>
2021-04-25 21:01:46 +0300

    удалил активноссть

--- Fait.DB/dbo/Tables/actual_groups.sql
+++ Fait.DB/dbo/Tables/actual_groups.sql
@@ -2,7 +2,6 @@
     [id]         INT IDENTITY (1, 1) NOT NULL,
     [group_id]   INT NULL,
     [student_id] INT NULL,
-    [actual]     BIT NOT NULL,
     PRIMARY KEY CLUSTERED ([id] ASC),
     FOREIGN KEY ([group_id]) REFERENCES [dbo].[groups] ([id]) ON DELETE SET DEFAULT,
     FOREIGN KEY ([student_id]) REFERENCES [dbo].[students] ([id]) ON DELETE SET DEFAULT
--- Fait.DB/dbo/Tables/groups.sql
+++ Fait.DB/dbo/Tables/groups.sql
@@ -2,6 +2,7 @@
     [id]         INT IDENTITY (1, 1) NOT NULL,
     [plan_id]    INT NULL,
     [group_name] INT NOT NULL,
+    [actual] BIT NOT NULL, 
     PRIMARY KEY CLUSTERED ([id] ASC),
     FOREIGN KEY ([plan_id]) REFERENCES [dbo].[year_plans] ([id]) ON DELETE SET NULL
 );
