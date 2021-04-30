87ad48e88f39105be74f25e7fac378e1477bc3fb
AlecCore <alocha2001@gmail.com>
2021-04-30 22:03:15 +0300

    Измениль модель карты студентов(добавление,трансфер).Поменял таблицу студентов, и DAL к ней

--- Fait.DAL/StudentsInfo.cs
+++ Fait.DAL/StudentsInfo.cs
@@ -20,6 +20,10 @@ namespace Fait.DAL
         public string Uniq { get; set; }
         public string NoCompetititon { get; set; }
         public byte? Ammends { get; set; }
+        public int EmploymentNumber { get; set; }
+        public string EmploymentAuthority { get; set; }
+        public DateTime EmploymentGivenDate { get; set; }
+        public string RegistrOrPassportNumber { get; set; }
 
         public virtual Student IdNavigation { get; set; }
     }
