579958e33798b33d996d6231e68751698511c181
Nastya <a.gurzhyi@gmail.com>
2021-05-01 13:11:51 +0300

    changes

---
+++ FaitLogic/StudentInfoLogic.cs
@@ -0,0 +1,51 @@
+﻿using AccessToDb;
+using Fait.DAL;
+using Fait.DTO;
+using System;
+
+namespace FaitLogic
+{
+    public class StudentInfoLogic
+    {
+        private AccessStudentInfo accessStudentInfo { get; set; } = new AccessStudentInfo();
+        public void AddStudentInfo(StudentCardDTO studentCard)
+        {
+            var isValid = ValidateStudentCard(studentCard);
+            if (!isValid)
+            {
+                return;
+            }
+
+            var studentInfo = new StudentsInfo
+            {
+                Birthdate = studentCard.Birthday,
+                BirthPlace = studentCard.BirthPlace,
+                Immenseness = studentCard.Immenseness,
+                //MaritalStatus = studentCard.MaritalStatus,
+                Registartion = studentCard.Registartion,
+                Exemption = studentCard.Exemption,
+                //Competition = studentCard.C,
+                //FromIns = studentCard.,
+                //Direction = studentCard.Birthday,
+                //Uniq = studentCard.OrderNumber,
+                //NoCompetititon = studentCard.Birthday,
+                //Ammends = studentCard.A,
+                EmploymentNumber = studentCard.EmploymentNumber,
+                EmploymentAuthority = studentCard.EmploymentAuthority,
+                //EmploymentGivenDate = studentCard.,
+                RegistrOrPassportNumber = studentCard.TaxPassportNumber
+            };
+
+            accessStudentInfo.AddStudentInfoToDb(studentInfo);
+        }
+
+        private bool ValidateStudentCard(StudentCardDTO studentCard)
+        {
+            if (studentCard.Name == null && studentCard.Name == "")
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
