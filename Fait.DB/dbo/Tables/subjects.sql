6ca31f01ec57cb016435aad6dc25d904db544478
Alex_Kopcha <alocha2001@gmail.com>
2021-05-18 19:24:32 +0300

    изменения прдеметов бд

--- Fait.DB/dbo/Tables/subjects.sql
+++ Fait.DB/dbo/Tables/subjects.sql
@@ -1,13 +1,10 @@
 ﻿CREATE TABLE [dbo].[subjects] (
     [id]         INT           IDENTITY (1, 1) NOT NULL,
-    [plan_id]    INT           NULL,
-    [sub_name]   NVARCHAR (30) NOT NULL,
-    [sub_hours]  INT           NOT NULL,
-    [ects]       INT           NOT NULL,
+    [subject_info_id] INT NOT NULL,
     [monitoring] BIT           DEFAULT ((0)) NOT NULL,
     [task]       BIT           DEFAULT ((0)) NOT NULL,
-    [semester]   TINYINT       NOT NULL,
+    [semester]   BIT       NOT NULL,
     PRIMARY KEY CLUSTERED ([id] ASC),
-    FOREIGN KEY ([plan_id]) REFERENCES [dbo].[year_plans] ([id]) ON DELETE SET DEFAULT
+    FOREIGN KEY ([subject_info_id]) REFERENCES [dbo].subject_info ([id]) ON DELETE SET DEFAULT
 );
 
