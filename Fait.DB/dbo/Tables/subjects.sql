675db7fe95bca2571256304d7699fc18b1651eec
Alex_Kopcha <alocha2001@gmail.com>
2021-05-19 12:23:02 +0300

    Добавил enum, поменял в предметах Bool/Bit на Byte/Tinyint

--- Fait.DB/dbo/Tables/subjects.sql
+++ Fait.DB/dbo/Tables/subjects.sql
@@ -1,9 +1,9 @@
 ﻿CREATE TABLE [dbo].[subjects] (
     [id]         INT           IDENTITY (1, 1) NOT NULL,
     [subject_info_id] INT NOT NULL,
-    [monitoring] BIT           DEFAULT ((0)) NOT NULL,
-    [task]       BIT           DEFAULT ((0)) NOT NULL,
-    [semester]   BIT       NOT NULL,
+    [monitoring] TINYINT           DEFAULT ((0)) NOT NULL,
+    [task]       TINYINT          DEFAULT ((0)) NOT NULL,
+    [semester]   TINYINT      NOT NULL,
     PRIMARY KEY CLUSTERED ([id] ASC),
     FOREIGN KEY ([subject_info_id]) REFERENCES [dbo].[subject_info] ([id])
 );
---
+++ FaitLogic/Enums/TaskEnum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FaitLogic.Enums
+{
+    public enum TaskEnum
+    {
+        NotExsist=0,
+        Exsist=1
+    }
+}
