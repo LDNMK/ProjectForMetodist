b3ac7d4104effc70c5a1d318b9ec0ffc3949fb87
Nastya <a.gurzhyi@gmail.com>
2021-06-01 21:59:21 +0300

    made readable comments in DAL and enums

--- FaitLogic/Enums/StudentStateEnum.cs
+++ FaitLogic/Enums/StudentStateEnum.cs
@@ -6,12 +6,12 @@ namespace FaitLogic.Enums
 {
     public enum StudentStateEnum
     {
-        DefaultTransfer = 1,//Перевод с группой
-        AcademicLeave = 2,// Студент в академ отпуске
-        TransferInAnotherGroup = 3,//Студент переходит в другую группу
-        NotClosedSession = 4,//Студент не закрыл сессию
-        StudentExpelled = 5,//Студент исключен
-        StudentGradueted = 6//Студент закончил  степень
+        DefaultTransfer = 1,        //Перевод с группой
+        AcademicLeave = 2,          // Студент в академ отпуске
+        TransferInAnotherGroup = 3, //Студент переходит в другую группу
+        NotClosedSession = 4,       //Студент не закрыл сессию
+        StudentExpelled = 5,        //Студент исключен
+        StudentGradueted = 6        //Студент закончил  степень
 
     }
 }
