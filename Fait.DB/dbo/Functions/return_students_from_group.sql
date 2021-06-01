beed7e131502b775d77ae7295ed7c441c78e2b65
Nastya <a.gurzhyi@gmail.com>
2021-06-01 22:00:45 +0300

    made readable sql queries

--- Fait.DB/dbo/Functions/return_students_from_group.sql
+++ Fait.DB/dbo/Functions/return_students_from_group.sql
@@ -1,16 +1,22 @@
 ﻿CREATE   FUNCTION return_students_from_group
 (
-@group_number INT,
-@group_name_id TINYINT
+@group_id INT
 )
 RETURNS TABLE
 AS
 RETURN
-SELECT id,(first_name+' '+last_name+' '+patronymic) full_name FROM students stud
-WHERE EXISTS(
-SELECT 1 FROM actual_groups ac_gr
-WHERE ac_gr.student_id=stud.id AND EXISTS(
-SELECT 1 FROM groups gr
-WHERE gr.id=ac_gr.group_id AND gr.group_number=@group_number AND gr.group_name_id=@group_name_id
-)
-)
+	SELECT id,
+	(first_name + ' ' + last_name + ' ' + patronymic) AS full_name 
+	FROM students stud
+	WHERE EXISTS (
+		SELECT 1 
+		FROM actual_groups ac_gr
+		WHERE ac_gr.student_id = stud.id 
+		AND EXISTS (
+			SELECT 1 
+			FROM groups gr
+			WHERE gr.id = ac_gr.group_id 
+			AND gr.id = @group_id
+			AND gr.actual = 1
+			)
+	)
--- Fait.DB/dbo/Stored Procedures/add_ammendes.sql
+++ Fait.DB/dbo/Stored Procedures/add_ammendes.sql
@@ -2,6 +2,10 @@
 CREATE PROCEDURE add_ammendes
 AS
 BEGIN
-INSERT INTO ammendes(id,ammend_type) 
-VALUES(1,'Інформація відсутня'),(2,'Державний кредит'),(3,'Фізична особа'),(4,'Юридична особа')
+	INSERT INTO ammendes(id, ammend_type) 
+	VALUES
+		(1, 'Інформація відсутня'),
+		(2, 'Державний кредит'),
+		(3, 'Фізична особа'),
+		(4, 'Юридична особа')
 END
