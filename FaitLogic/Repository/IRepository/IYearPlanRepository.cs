1030dbe9a803c08d0ffcc0fccd2c9427f029c6b0
Nastya <a.gurzhyi@gmail.com>
2021-06-03 19:06:25 +0300

    added new repositories and their interfaces

---
+++ FaitLogic/Repository/IRepository/IYearPlanRepository.cs
@@ -0,0 +1,16 @@
+﻿using Fait.DAL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FaitLogic.Repository.IRepository
+{
+    public interface IYearPlanRepository
+    {
+        List<YearPlan> GetListOfYearPlans(int course);
+
+        int? AddYearPlan(YearPlan yearPlan);
+
+        YearPlan FindYearPlan(int yearPlanId);
+    }
+}
---
+++ FaitLogic/Repository/SubjectRepository.cs
@@ -0,0 +1,47 @@
+﻿using Fait.DAL;
+using FaitLogic.Repository.IRepository;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FaitLogic.Repository
+{
+    public class SubjectRepository : ISubjectRepository
+    {
+        private readonly FAIT4Context dbContext;
+
+        public SubjectRepository(FAIT4Context context)
+        {
+            dbContext = context;
+        }
+
+        public List<SubjectInfo> FindSubjectsInfo(int yearPlanId)
+        {
+            return dbContext.SubjectInfos
+                .Where(x => x.PlanId == yearPlanId)
+                .ToList();
+        }
+
+        public List<Subject> FindSubjects(int subjectId)
+        {
+            return dbContext.Subjects
+                .Where(x => x.SubjectInfoId == subjectId)
+                .ToList();
+        }
+
+        public int? AddSubjectInfo(SubjectInfo subjectInfo)
+        {
+            dbContext.SubjectInfos.Add(subjectInfo);
+            dbContext.SaveChanges();
+
+            return dbContext.SubjectInfos
+                .OrderBy(x => x.Id)
+                .LastOrDefault()?.Id;
+        }
+
+        public void AddSubject(Subject subject)
+        {
+            dbContext.Subjects.Add(subject);
+            dbContext.SaveChanges();
+        }
+    }
+}
