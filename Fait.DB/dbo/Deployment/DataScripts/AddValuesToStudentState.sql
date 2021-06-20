77efacbf7f4b2e19950ab93f1444eac11e4d290a
Nastya <a.gurzhyi@gmail.com>
2021-06-20 13:34:09 +0300

    post deployment script

--- Fait.DB/dbo/Deployment/DataScripts/AddValuesToStudentState.sql
+++ Fait.DB/dbo/Deployment/DataScripts/AddValuesToStudentState.sql
@@ -1,13 +1,23 @@
 ﻿
-CREATE PROCEDURE AddValuesToStudentState
-AS
-BEGIN
-	INSERT INTO student_states (id, student_state_name) 
-	VALUES
-		(1, 'Перехід з групою'),
-		(2, 'Академічна відпустка'),
-		(3, 'Перехід в іншу группу'),
-		(4, 'Не закрита сесія'),
-		(5, 'Студент відрахований'),
-		(6, 'Студент отримав ступінь')
-END
+SET IDENTITY_INSERT [student_states] ON
+
+MERGE INTO [student_states] AS o
+USING (SELECT *
+	FROM
+	(
+		VALUES
+			(1, 'Перехід з групою'),
+			(2, 'Академічна відпустка'),
+			(3, 'Перехід в іншу группу'),
+			(4, 'Не закрита сесія'),
+			(5, 'Студент відрахований'),
+			(6, 'Студент отримав ступінь')
+	)
+	SOURCE (Id, [student_state_name])) AS n
+ON o.Id = n.Id
+WHEN NOT MATCHED BY TARGET THEN
+	INSERT (Id, [student_state_name])
+	VALUES (n.Id, n.[student_state_name]);
+
+SET IDENTITY_INSERT [student_states] OFF
+GO
--- Fait.DB/dbo/Tables/marital_statuses.sql
+++ Fait.DB/dbo/Tables/marital_statuses.sql
@@ -1,5 +1,5 @@
 ﻿CREATE TABLE [dbo].[marital_statuses] (
-    [id]                  TINYINT       NOT NULL,
+    [id]                  TINYINT    IDENTITY(1, 1)   NOT NULL,
     [marital_status_name] NVARCHAR (40) NULL,
     PRIMARY KEY CLUSTERED ([id] ASC)
 );
