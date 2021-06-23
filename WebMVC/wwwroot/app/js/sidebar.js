5e5e8c54d15c61ba606e6d4f96d5df5ff91e3381
Vlad Sydorchuk <vladsydorchuk.ln@gmail.com>
2021-06-23 09:09:37 +0300

    Added properties to sidebar and did it as fixed

--- WebMVC/wwwroot/app/js/sidebar.js
+++ WebMVC/wwwroot/app/js/sidebar.js
@@ -1,10 +1,15 @@
 function initSidebar() {
     let pages = document.body.querySelectorAll("a[data-page*='-page'");
+    let items = document.body.querySelectorAll(".sidebar__menu-item");
     
     pages.forEach(x => {
         x.addEventListener('click', (e) => {
             let attr = e.target.getAttribute('data-page');
             setPageByAttribute(attr);
+
+
+            items.forEach(item => item.classList.remove('active'));
+            e.target.closest('.sidebar__menu-item').classList.add('active');
         });
     });
 }
