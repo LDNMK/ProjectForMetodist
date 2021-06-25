ab2b3f7c9b6c8aefcbf8278bbc656f7e3d1f0bd8
Nastya <a.gurzhyi@gmail.com>
2021-06-25 09:16:56 +0300

    partial implementation of progress page

---
+++ FaitLogic/DTO/ProgressDTO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FaitLogic.DTO
+{
+    public class ProgressDTO
+    {
+        public ICollection<ProgressSubjectDTO> Subjects { get; set; }
+        public ICollection<ProgressStudentDTO> Students { get; set; }
+    }
+
+    public class ProgressSubjectDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class ProgressStudentDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<SubjectStudentMarkDTO> Subjects { get; set; }
+    }
+
+    public class SubjectStudentMarkDTO
+    {
+        public int Id { get; set; }
+        public int Mark { get; set; }
+    }
+}
---
+++ WebMVC/Controllers/ProgressController.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using FaitLogic.DTO;
+using FaitLogic.Logic;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ProgressController : ControllerBase
+    {
+        private readonly IMapper mapper;
+
+        private readonly ProgressLogic progressLogic;
+
+        public ProgressController(IMapper mapper, ProgressLogic progressLogic)
+        {
+            this.mapper = mapper;
+            this.progressLogic = progressLogic;
+        }
+
+        [HttpGet]
+        public IActionResult GetProgress(int year, int groupId, int semesterId)
+        {
+            var progress = progressLogic.GetProgress(year, groupId, semesterId);
+
+            return Ok(progress);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateProgress([FromBody] ProgressModel progressModel)
+        {
+            var progressDto = mapper.Map<ProgressModel, ProgressDTO>(progressModel);
+
+            progressLogic.UpdateProgress(progressDto);
+
+            return Ok();
+        }
+    }
+}
