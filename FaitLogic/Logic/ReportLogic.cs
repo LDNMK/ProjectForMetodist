f2c1b7a57651fb388d06c1c285b0556a19569164
Nastya <a.gurzhyi@gmail.com>
2021-07-09 20:03:31 +0300

    small partial implementation of report creation

---
+++ FaitLogic/Logic/ReportLogic.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Fait.DAL.Repository.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FaitLogic.Logic
+{
+    public class ReportLogic
+    {
+        private readonly IMapper _mapper;
+
+        private readonly IUnitOfWork unitOfWork;
+
+        public ReportLogic(
+            IMapper mapper,
+            IUnitOfWork unitOfWork)
+        {
+            _mapper = mapper;
+            this.unitOfWork = unitOfWork;
+        }
+
+        public void CreateReport(int studentId)
+        {
+
+        }
+
+        private void CreateDocument()
+        {
+
+        }
+    }
+}
